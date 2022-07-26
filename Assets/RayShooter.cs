fd0e6bb7b1c02beeab4dcb310979d47f2b3da161
mephex <gitlab@spamfree4now.com>
2022-07-26 12:09:03 -0500

    +Spheres

---
+++ Assets/RayShooter.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+    private Camera cam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 point = new Vector3(cam.pixelWidth/2, cam.pixelHeight/2, 0);
+            Ray ray = cam.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                StartCoroutine(SphereIndicator(hit.point));
+            }
+        }
+    }
+    
+    private IEnumerator SphereIndicator(Vector3 pos) {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+
+        yield return new WaitForSeconds(1);
+
+        Destroy(sphere);
+    }
+}
+
